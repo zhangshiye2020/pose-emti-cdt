ecf763f113110cd22ef25b93da6731064cb5a455
zhangshiye2020 <zhangshiye2020@gmail.com>
2021-10-10 00:05:20 +0800

    v1.0

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include "circledec.h"
+#include <time.h>
+
+using namespace std;
+/*
+ * 聚类算法分类，但是效果不一定好
+ */
+void filterContoursByArea(cv::Mat contourAreas, int classes) {
+    // 聚类算法
+    cv::Mat labels;
+    cv::kmeans(contourAreas, classes, labels,
+               cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 10, 0), 5, cv::KMEANS_PP_CENTERS);
+
+    for (int i = 0; i < labels.rows; i++) {
+        cout << " " << labels.at<int>(i, 0);
+    }
+    cout << endl;
+    for (int i = 0; i < contourAreas.rows; i++) {
+        cout << " " << contourAreas.at<float>(i, 0);
+    }
+}
+
+int main(int argc, char **argv) {
+	string folderpath = "/home/zhangshiye2020/图片/";
+	string filenameNoExtension = "target";
+	string extensionName = ".png";
+	string filepath = folderpath + filenameNoExtension + extensionName;
+    cv::Mat mat = cv::imread(filepath);
+    cv::Mat dst, pre_src;
+    pretreatment(mat, pre_src);
+    vector<CircleType> circles;
+    findCircleByContours(pre_src, circles);
+
+    return 0;
+}
